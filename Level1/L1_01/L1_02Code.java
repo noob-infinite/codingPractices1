91e7ac3f1e521337ae4439a99b09c5dbaa53b888
noob <compsuntana@gmail.com>
2022-06-27 10:34:55 +0700

    create Q2 eggsNeeded

--- Level1/L1_01/L1_02Code.java
+++ Level1/L1_01/L1_02Code.java
@@ -1,4 +1,4 @@
-class L1_01Code {
+class L1_02Code {
     // Given a varaible named year
     // break each digit of year into d1 d2 d3 d4 from left to right
     public static void main(String... args) {
@@ -6,13 +6,6 @@ class L1_01Code {
         int number = year;
         int d1, d2, d3, d4;
         d1 = d2 = d3 = d4 = 0;
-
-        d1 = number / 1000;
-        number = number % 1000;
-        d2 = number / 100;
-        number = number % 100;
-        d3 = number / 10;
-        d4 = number % 10;
 
         System.out.println(year + " = " + d1 + " " + d2 + " " + d3 + " " + d4);
     }
--- Level1/L1_01/README.md
+++ Level1/L1_01/README.md
@@ -1,4 +1,13 @@
+Q01
+Given 3 variables named
+numEggsPerBatch, numBatch, eggsNeeded
+write an expression such that
+eggsNeeded is the multiplication of numEggsPerBatch and numBatch
+
+Q02 
 Given a varaible named year
-break each digit of year into digit1 digit2 digit3 digit4 from left to right
+break each digit of year into d1 d2 d3 d4 from left to right
+
+
 
 adapted from https://codeforces.com/problemset/problem/271/A
